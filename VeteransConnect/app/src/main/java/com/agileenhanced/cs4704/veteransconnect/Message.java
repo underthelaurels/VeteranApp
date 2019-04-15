bc5d60370029e1bbf483b1414eb3c435d0a42129
Shawn Singh <shawn26s@vt.edu>
2019-04-15 13:45:11 -0500

    IRC set up. one chat room is enabled and active.

---
+++ VeteransConnect/app/src/main/java/com/agileenhanced/cs4704/veteransconnect/Message.java
@@ -0,0 +1,30 @@
+package com.agileenhanced.cs4704.veteransconnect;
+
+public class Message
+{
+    private String text;
+    private MemberData memberData;
+    private boolean belongsToCurrentUser;
+
+    public Message(String text, MemberData data, boolean belongsToCurrentUser)
+    {
+        this.text = text;
+        this.memberData = data;
+        this.belongsToCurrentUser = belongsToCurrentUser;
+    }
+
+    public String getText()
+    {
+        return text;
+    }
+
+    public MemberData getMemberData()
+    {
+        return memberData;
+    }
+
+    public boolean isBelongsToCurrentUser()
+    {
+        return belongsToCurrentUser;
+    }
+}
